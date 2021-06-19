081763ef7616e22b4aefae0b3108b7c7c0d004ed
Javier Torres <javierandrestorresreyes@gmail.com>
2021-06-18 22:09:52 -0500

    envia mensaje desde el servicio rest, solo envia el ultimo mensaje

--- src/main/java/com/example/chatdemo/controller/RestController.java
+++ src/main/java/com/example/chatdemo/controller/RestController.java
@@ -3,12 +3,11 @@ package com.example.chatdemo.controller;
 import com.example.chatdemo.entity.A00352115messages;
 import com.example.chatdemo.repository.A00352115messagesRepository;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.*;
 
 
+import java.util.Date;
 import java.util.List;
 
 @org.springframework.web.bind.annotation.RestController
@@ -16,13 +15,23 @@ import java.util.List;
 public class RestController {
 
     private A00352115messagesRepository repository;
+    private SimpMessagingTemplate messagingTemplate;
 
-    public RestController(A00352115messagesRepository repository){
+    public RestController(A00352115messagesRepository repository, SimpMessagingTemplate messagingTemplate){
         this.repository = repository;
+        this.messagingTemplate = messagingTemplate;
     }
 
     @GetMapping(value = "/damelos/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<A00352115messages> damelos(@PathVariable String id){
         return repository.findByChatId(id);
+    }
+
+    @PostMapping(value = "/enviar/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public A00352115messages damelos(@PathVariable String id, @RequestBody A00352115messages msg){
+        msg.setCreatedAt(new Date());
+        A00352115messages result = repository.saveAndFlush(msg);
+        messagingTemplate.convertAndSendToUser(result.getChatId(), "/messagesInd", result);
+        return result;
     }
 }
